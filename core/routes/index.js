21be6836377e7802b1161ce1325f067bd33fdafc
Jared Mullin <JaredMullin@ProtonMail.com>
2019-09-25 10:21:23 +0100

    Fixed Routing Issue

--- core/routes/index.js
+++ core/routes/index.js
@@ -4,14 +4,12 @@ const spotify = require('../controllers/spotify');
 
 const router = express.Router();
 
-router.get('/api/spotify/userProfile', spotify.UserProfile);
-
 router.get('/api/spotify/auth', spotify.Auth);
 
 router.get('/api/spotify/callback', spotify.Callback);
 
 router.get('/api/spotify/refresh', spotify.Refresh);
 
-router.get('/api/spotify/refresh', spotify.RetrieveArtists);
+router.get('/api/spotify/retrieveArtists', spotify.RetrieveArtists);
 
 module.exports = router;
