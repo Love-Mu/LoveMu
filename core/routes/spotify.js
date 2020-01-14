7ee05e6455ea1cf2d84036415cae90df2a27c41a
Jared-Mullin <JaredMullin@protonmail.com>
2020-01-14 12:49:08 +0000

    Fixed security vulnerability

--- core/routes/spotify.js
+++ core/routes/spotify.js
@@ -4,12 +4,12 @@ const Spotify = require('../controllers/spotify');
 const Assets = require('../assets/miscAssets');
 const router = express.Router();
 
-router.get('/reqAccess', Assets.checkSession, Spotify.requestAccess);
+router.get('/reqAccess', Spotify.requestAccess);
 
 router.get('/reqCallback', Spotify.callbackAccess);
 
-router.get('/refToken', Assets.checkSession, Spotify.refreshAccess);
+router.get('/refToken', Spotify.refreshAccess);
 
-router.get('/retArtists', Assets.checkSession, Spotify.retrieveArtists);
+router.get('/retArtists', Spotify.retrieveArtists);
 
 module.exports = router;
