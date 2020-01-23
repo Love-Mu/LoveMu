91d4d792eced36273a6c491dcaab7cf56bbbbb77
Jared-Mullin <JaredMullin@protonmail.com>
2020-01-23 14:54:37 +0000

    DB call inserted into profile controller

--- core/controllers/profileController.js
+++ core/controllers/profileController.js
@@ -7,39 +7,40 @@ module.exports = {
     /* Insert DB call to return all profiles and store in array,
     ensure we don't send password, can be used to retrieve
     sexuality/gender in future*/
-    // Current user _id can be retrieved with req.session.passport.user
-    const users = {};
-    const currUser = User.genres;
-    const usrGenreArr = {};
-    const usrScore = {};
-    currUser.forEach((val, key, map) => {
-      usrGenreArr.push(key);
-      usrScore.push(val);
-    });
-    users.forEach((usr) => {
-      const tempScore = usrScore;
-      const tempUsrScore = {};
-      const currGenres = usr.genres;
-      usrGenreArr.forEach((val) => {
-        if (!usr.has(val)) {
-          tempUsrScore.push(0);
-        } else {
-          tempUsrScore.push(currGenres.get(val));
-        }
+    // Current user _id can be retrieved with req.user
+    User.find({}).exec((err, users) => {
+      const currUser = req.user.genres;
+      const usrGenreArr = {};
+      const usrScore = {};
+      currUser.forEach((val, key, map) => {
+        usrGenreArr.push(key);
+        usrScore.push(val);
       });
-      currGenres.forEach((val, key, map) => {
-        if (!usrGenreArr.includes(key)) {
-          tempScore.push(0);
-          tempUsrScore.push(currGenres.get(key));
-        }
+      users.forEach((usr) => {
+        const tempScore = usrScore;
+        const tempUsrScore = {};
+        const currGenres = usr.genres;
+        usrGenreArr.forEach((val) => {
+          if (!usr.has(val)) {
+            tempUsrScore.push(0);
+          } else {
+            tempUsrScore.push(currGenres.get(val));
+          }
+        });
+        currGenres.forEach((val, key, map) => {
+          if (!usrGenreArr.includes(key)) {
+            tempScore.push(0);
+            tempUsrScore.push(currGenres.get(key));
+          }
+        });
+        usr.score = similarity(tempScore, tempUsrScore);
       });
-      usr.score = similarity(tempScore, tempUsrScore);
+      res.json(users.sort((a, b) => (a.score >= b.score) ? 1 : -1));
     });
-    res.json(users.sort((a, b) => (a.score >= b.score) ? 1 : -1));
   },
   getProfile: (req, res, next) => {
     const uId = req.params.id;
-    // Find user based on this idea and serve it
+    // Find user based on this id and serve it
     res.json(usr);
   },
 };
