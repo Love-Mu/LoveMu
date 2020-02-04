a3143cd087f44bb8263edd64d4082b812a0cc78b
Laura <Laura.OD@live.com>
2020-02-04 21:27:08 +0000

    Modified User schema

--- core/models/User.js
+++ core/models/User.js
@@ -4,23 +4,72 @@ const passport = require('passport');
 
 const Schema = mongoose.Schema;
 
+
+
 // Define User model here
 const userSchema = new Schema({
-  email: {type: String},
-  password: {type: String},
-  access_token: {type: String},
-  refresh_token: {type: String},
-  artists: {type: Array},
-  genres: {type: Map},
-  user_name: {type: String, default: ""},
-  fname: {type: String, default: ""},
-  sname: {type: String, default: ""}, 
-  date_of_birth: {type: Date},
-  location: {type: String, default: ""},
-  image: {type: String, default : ""}, // temporary, need to replace with actual image storage
-  gender: {type: String, default: ""},
-  sexuality: {type: String, default: ""},
-  bio: {type: String, default: ""}
+  email: {
+    type: String, 
+    unique: true, 
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+    select: false
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is Required",
+    select: false
+  },
+  access_token: {
+    type: String,
+    select: false
+  },
+  refresh_token: {
+    type: String,
+    select: false
+  },
+  artists: {
+    type: Array
+  },
+  genres: {
+    type: Map
+  },
+  user_name: {
+    type: String, 
+    default: ""
+  },
+  fname: {
+    type: String, 
+    default: ""
+  },
+  sname: {
+    type: String, 
+    default: "",
+    select: false
+  }, 
+  date_of_birth: {
+    type: Date
+  },
+  location: {
+    type: String, 
+    default: ""
+  },
+  image: {
+    type: String, 
+    default : ""
+  }, // temporary, need to replace with actual image storage
+  gender: {
+    type: String, 
+    default: ""
+  },
+  sexuality: {
+    type: String, 
+    default: ""
+  },
+  bio: {
+    type: String, 
+    default: ""
+  }
 });
 
 userSchema.methods.hashPassword = function(password) {
