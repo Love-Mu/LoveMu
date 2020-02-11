97c690c60550167caf5fbd78f3444fb038f73ef4
Jared-Mullin <JaredMullin@protonmail.com>
2020-02-11 13:07:38 +0000

    Fixed missing ']'

--- core/routes/authentication.js
+++ core/routes/authentication.js
@@ -16,7 +16,7 @@ router.post('/login',  userValidationRules(), validate, passport.authenticate('l
 
 router.get('/google', passport.authenticate('google', { scope: 
     [ 'https://www.googleapis.com/auth/plus.login',
-    , 'https://www.googleapis.com/auth/plus.profile.emails.read'}
+    , 'https://www.googleapis.com/auth/plus.profile.emails.read']},
 ));
 
 router.get('/googleCallback', passport.authenticate('google', {
