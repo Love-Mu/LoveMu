36fa87d99ef891cd60f4b5a796b9f923f3bf4389
Jared-Mullin <JaredMullin@protonmail.com>
2020-02-13 14:38:44 +0000

    Fixed missing onSubmit method

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { AuthenticationService } from '../authentication.service';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
