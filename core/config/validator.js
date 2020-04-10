ce196b2dedc4c4f2c5213b0688a65a1afabe840e
root <root@lovemu.compsoc.ie>
2020-04-10 14:30:28 +0100

    Last minute edits

--- core/config/validator.js
+++ core/config/validator.js
@@ -2,13 +2,13 @@ const {body, validationResult} = require('express-validator');
 
 module.exports = {
   registrationValidationRules: () => {
-    return [body('email').isEmail(), body('password').isLength({min: 5}), body('user_name').trim().isLength({min: 5, max:20}).isWhitelisted("_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"), body('gender').notEmpty().trim(), body('sexuality').notEmpty().trim(), body('dob').notEmpty().trim(), body('fname').notEmpty().trim(), body('sname').trim().notEmpty(), body('location').notEmpty().trim(), body('bio').trim().isLength({min:3, max:250})];
+    return [body('email').isEmail().trim().isLength({min: 5, max:60}), body('password').isLength({min: 5}), body('user_name').trim().isLength({min: 5, max:20}).isWhitelisted("_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"), body('gender').notEmpty().trim(), body('sexuality').notEmpty().trim(), body('dob').notEmpty().trim(), body('fname').notEmpty().trim(), body('sname').trim().notEmpty(), body('location').notEmpty().trim(), body('bio').trim().isLength({min:3, max:250})];
   },
   updateValidationRules: () => {
     return [body('user_name').trim().isLength({min: 5, max: 20}), body('gender').notEmpty(), body('sexuality').notEmpty(), body('dob').notEmpty(),body('fname').notEmpty(), body('sname').trim().notEmpty(), body('location').notEmpty().trim()];
   },
   loginValidationRules: () => {
-    return [body('email').trim().isLength({min: 5, max:20}), body('password').isLength({min: 5})];
+    return [body('email').trim().isLength({min: 5, max:60}), body('password').isLength({min: 5})];
   },
   messageValidationRules: () => {
     return [body('body').trim().isLength({min: 1, max:2000})];
